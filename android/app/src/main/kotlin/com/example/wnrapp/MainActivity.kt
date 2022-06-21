20403a2cca6e8a5b20483632aaf0fb86eed0a8e0
A-F <68660959+sawargicms@users.noreply.github.com>
2022-06-21 17:45:33 +0700

    Copy project files

---
+++ android/app/src/main/kotlin/com/example/wnrapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wnrapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/navigation_item.dart
@@ -0,0 +1,14 @@
+import 'package:wnrapp/enum/action_type.dart';
+
+class NavigationItem {
+  final String name;
+  final String icon;
+  final ActionType type;
+  final String value;
+
+  NavigationItem(
+      {required this.name,
+      required this.icon,
+      required this.type,
+      required this.value});
+}
